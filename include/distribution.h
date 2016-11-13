5f6764d633ab622760a40ba57222b9a648164398
Emmanuel Hadoux <emmanuel.hadoux@gmail.com>
2016-11-13 14:42:20 +0000

    Fully working version.

--- include/distribution.h
+++ include/distribution.h
@@ -2,7 +2,9 @@
 #define SPLITTERCELL_DISTRIBUTION_H
 
 #include <vector>
-#include <map>
+#include <deque>
+#include <set>
+#include <unordered_map>
 #include <utility>
 #include <iostream>
 #include <limits>
@@ -10,84 +12,275 @@
 #include <cmath>
 #include <boost/dynamic_bitset.hpp>
 #include <sstream>
+#include <memory>
+#include <future>
+#include <algorithm>
 
 namespace splittercell {
-    struct Flock {
-        Flock(std::initializer_list<unsigned int> args, std::initializer_list<unsigned int> cond) :
-                conditioned(args), conditioning(cond){}
-        std::vector<unsigned int> conditioned, conditioning;
+    class Flock {
+    public:
+        /* Constructors */
+        Flock(const std::vector<unsigned int> &args, const std::vector<unsigned int> &cond = {}, const std::vector<double> &distribution = {});
+        /* Accessors */
+        unsigned int size() const {return _size;}
+        const std::vector<double> &distribution() const {return _distribution;}
+        void set_probabilities(const std::vector<double> &probabilities) {_distribution = probabilities;}
+        const std::vector<unsigned int> &conditioned() const {return _conditioned;}
+        const std::vector<unsigned int> &conditioning() const {return _conditioning;}
+        /* Modifiers*/
+        void refine(unsigned int argument, bool positive, double coefficient);
+        std::shared_ptr<Flock> marginalize(const std::vector<unsigned int> &args_to_keep) const;
+        void marginalize_self(const std::vector<unsigned int> &args_to_keep);
+        std::shared_ptr<Flock> combine(const std::shared_ptr<Flock> &flock) const;
+
+        std::string to_str() const;
+        bool operator==(const Flock &other) const {return (_conditioned == other._conditioned) &&
+                    (_conditioning == other._conditioning) && (_distribution == other._distribution); }
+
+    private:
+        std::vector<unsigned int> _conditioned, _conditioning;
+        std::vector<double> _distribution;
+        std::unordered_map<unsigned int, unsigned int> _mapping;
+        unsigned int _size;
+
+        void map_arguments();
+        std::vector<double> marginalized_distribution(const std::vector<unsigned int> &args_to_keep) const;
     };
+
+    /********************** Implementation ************************/
+
+    Flock::Flock(const std::vector<unsigned int> &args, const std::vector<unsigned int> &cond, const std::vector<double> &distribution) :
+            _conditioned(args), _conditioning(cond), _distribution(distribution), _size(args.size() + cond.size()) {
+        unsigned int limit = std::numeric_limits<unsigned int>::digits;
+        if(_size > (limit - 2))
+            throw std::overflow_error("Too many arguments in the flock.");
+        unsigned int num_of_models = (unsigned int)(1 << _size);
+        double initial_belief      = 1.0 / (1 << _size);
+        if(_distribution.empty())
+            _distribution          = std::vector<double>(num_of_models, initial_belief);
+
+        map_arguments();
+    }
+
+    std::string Flock::to_str() const {
+        std::stringstream ss;
+        for (double val : _distribution) ss << val << " ";
+        std::string s = ss.str();
+        s.pop_back();
+        return s;
+    }
+
+    void Flock::refine(unsigned int argument, bool positive, double coefficient) {
+        unsigned int index = _mapping[argument];
+        if(index >= _conditioned.size())
+            throw std::invalid_argument("Only conditioned arguments can be refined.");
+        for (unsigned int i = 0; i < _distribution.size(); i++) {
+            boost::dynamic_bitset<> binary(_size, i); //Current model in binary notation
+            if (binary.test(index) == positive) { //If the model satisfies argument+side of update
+                unsigned int opposite = (unsigned int) binary.flip(index).to_ulong(); //Closest model not satisfying
+                double opposite_val = _distribution[opposite];
+                _distribution[opposite] *= (1 - coefficient);
+                _distribution[i] += coefficient * opposite_val;
+            }
+        }
+    }
+
+    std::vector<double> Flock::marginalized_distribution(const std::vector<unsigned int> &args_to_keep) const {
+        /* New mapping creation (because marginalization put holes in the previous one) */
+        unsigned int index = 0;
+        std::map<unsigned int, unsigned int> mapping;
+        if(args_to_keep == _conditioned)
+            return _distribution;
+        for(unsigned int arg : args_to_keep)
+            if(_mapping.find(arg) != _mapping.end())
+                mapping[_mapping.at(arg)] = index++;
+        for(unsigned int arg : _conditioning)
+            if(_mapping.find(arg) != _mapping.end() && mapping.find(_mapping.at(arg)) == mapping.cend())
+                mapping[_mapping.at(arg)] = index++;
+
+        /* Actual marginalization */
+        unsigned int marginalized_size = (unsigned int)(1 << mapping.size());
+        auto distribution = std::vector<double>(marginalized_size, 0.0);
+        for(unsigned int i = 0; i < _distribution.size(); i++) {
+            boost::dynamic_bitset<> start(_size, i), end(mapping.size(), 0);
+            for(auto map : mapping)
+                end.set(map.second, start[map.first]);
+            distribution[end.to_ulong()] += _distribution[i];
+        }
+        return distribution;
+    }
+
+    std::shared_ptr<Flock> Flock::marginalize(const std::vector<unsigned int> &args_to_keep) const {
+        return std::make_shared<Flock>(args_to_keep, _conditioning, marginalized_distribution(args_to_keep));
+    }
+
+    void Flock::marginalize_self(const std::vector<unsigned int> &args_to_keep) {
+        _conditioned  = args_to_keep;
+        _distribution = marginalized_distribution(args_to_keep);
+        map_arguments();
+    }
+
+    std::shared_ptr<Flock> Flock::combine(const std::shared_ptr<Flock> &flock) const {
+        /* Combined flock creation */
+        std::vector<unsigned int> conditioned, conditioning;
+        conditioned.insert(conditioned.end(), _conditioned.cbegin(), _conditioned.cend());
+        conditioned.insert(conditioned.end(), flock->_conditioned.cbegin(), flock->_conditioned.cend());
+        /* Remove the conditioning arguments that are conditioned in the other flock (Bayes' rule) */
+        std::copy_if(_conditioning.cbegin(), _conditioning.cend(), std::back_inserter(conditioning),
+                     [flock](unsigned int i){return std::find(flock->_conditioned.cbegin(), flock->_conditioned.cend(), i) == flock->_conditioned.cend();});
+        std::copy_if(flock->_conditioning.cbegin(), flock->_conditioning.cend(), std::back_inserter(conditioning),
+                     [this](unsigned int i){return std::find(this->_conditioned.cbegin(), this->_conditioned.cend(), i) == this->_conditioned.cend();});
+        auto combinedflock = std::make_shared<Flock>(conditioned, conditioning);
+        unsigned int limit = std::numeric_limits<unsigned int>::digits - 2;
+        if(combinedflock->size() > limit)
+            throw std::overflow_error("Too many arguments in the final combined flock.");
+
+        /* Mapping between combined flock indexes and split flock index */
+        std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> splitindex;
+        conditioned.insert(conditioned.end(), conditioning.cbegin(), conditioning.cend()); //Just to have the whole set of arguments
+        for(auto arg : conditioned) {
+            auto itindexself = _mapping.find(arg);
+            unsigned int indexself = (itindexself == _mapping.end()) ? 0 : itindexself->second + 1; //Careful, mandatory to keep unsigned
+            auto itindexother = flock->_mapping.find(arg);
+            unsigned int indexother = (itindexother == flock->_mapping.end()) ? 0 : itindexother->second + 1; //Careful
+            splitindex.emplace(combinedflock->_mapping[arg], std::make_pair<>(indexself, indexother));
+        }
+
+        /* Actual combination */
+        for(unsigned int i = 0; i < (unsigned int)(1 << combinedflock->size()); i++) {
+            boost::dynamic_bitset<> start(combinedflock->size(), i), end1(_size, 0), end2(flock->size(), 0);
+            for(unsigned int j = 0; j < start.size(); j++) {
+                auto indexes = splitindex[j];
+                if(indexes.first != 0)
+                    end1[indexes.first-1] = start[j];
+                if(indexes.second != 0)
+                    end2[indexes.second-1] = start[j];
+            }
+            combinedflock->_distribution[i] = _distribution[end1.to_ulong()] * flock->_distribution[end2.to_ulong()];
+        }
+        return combinedflock;
+    }
+
+    /* Mapping argument <-> index to be (somewhat) order agnostic, conditioned first, then conditioning */
+    void Flock::map_arguments() {
+        unsigned int index = 0;
+        for(unsigned int arg : _conditioned)
+            _mapping[arg] = index++;
+        for(unsigned int arg : _conditioning)
+            _mapping[arg] = index++;
+    }
+
+    /*************************************************************************************************************************************/
 
     class Distribution {
     public:
-        Distribution(const std::vector<Flock> &flocks);
-
-        void refine(unsigned int argument, bool positive, double coefficient);
-
-        void set_flock_probabilities(unsigned int flock, const std::vector<double> &probabilities) { _distribution[flock] = probabilities; }
-        const std::vector<std::vector<double>>& distribution() const { return _distribution; }
+        /* Constructors */
+        Distribution(const std::vector<std::shared_ptr<Flock>> &flocks);
+        /* Accessors */
+        std::unordered_map<unsigned int, double> operator[](const std::vector<unsigned int> &arguments) const;
+        void set_probabilities(unsigned int flock, const std::vector<double> &probabilities) {
+            _flocks[flock]->set_probabilities(probabilities);
+            for(auto arg : _flocks[flock]->conditioned())
+                _cache_is_valid[arg] = false;
+        }
+        std::shared_ptr<Flock> flock(unsigned int flock) const {return _flocks.at(flock);}
+        /* Modifiers */
+        void refine(unsigned int argument, bool positive, double coefficient) {
+            _flocks[_mapping[argument]]->refine(argument, positive, coefficient);
+            _cache_is_valid[argument] = false;
+        }
+        std::shared_ptr<Flock> marginalize(unsigned int flock, const std::vector<unsigned int> &args_to_keep) {
+            return _flocks[flock]->marginalize(args_to_keep);
+        }
 
         std::string to_str() const;
 
-        double operator[](const std::string &model) const;
-        double operator[](const unsigned int &argument) const;
-
     private:
-        std::vector<std::vector<double>> _distribution;
-        std::map<unsigned int, std::pair<unsigned int, unsigned int>> _mapping;
-        std::map<unsigned int, unsigned int> _sizes;
+        std::vector<std::shared_ptr<Flock>> _flocks;
+        std::unordered_map<unsigned int, unsigned int> _mapping;
+        std::unordered_map<unsigned int, double> _belief_cache;
+        std::unordered_map<unsigned int, bool> _cache_is_valid;
+
+        void find_conditioning(unsigned int argument, std::set<unsigned int> &conditioning) const;
+        std::shared_ptr<Flock> find_and_combine(const std::vector<unsigned int> &arguments) const;
     };
 
-    Distribution::Distribution(const std::vector<Flock> &flocks) :
-            _distribution(flocks.size()) {
+    /********************** Implementation ************************/
+
+    Distribution::Distribution(const std::vector<std::shared_ptr<Flock>> &flocks) : _flocks(flocks) {
         unsigned int flock_index = 0;
-        for (auto flock : flocks) {
-            unsigned int arg_index = 0, flock_size = (unsigned int) (flock.conditioned.size() + flock.conditioning.size());
-            if (flock_size > std::numeric_limits<unsigned int>::digits)
-                throw std::overflow_error("Too many arguments in the flock.");
-            _sizes.emplace(flock_index, flock_size);
-            for (auto conditioned : flock.conditioned) {
+        for (auto flock : _flocks) {
+            for (auto conditioned : flock->conditioned()) {
                 if (_mapping.find(conditioned) != _mapping.end())
                     throw std::invalid_argument("An argument cannot be in different flocks.");
-                _mapping.emplace(conditioned, std::make_pair(flock_index, arg_index));
-                arg_index++;
+                _mapping[conditioned] = flock_index;
+                _belief_cache[conditioned] = 0.5;
+                _cache_is_valid[conditioned] = true;
             }
-            _distribution[flock_index] = std::vector<double>((unsigned int)(1 << flock_size), 1.0 / (1 << flock_size));
             flock_index++;
         }
     }
 
-    void Distribution::refine(unsigned int argument, bool positive, double coefficient) {
-        unsigned int flock_ind, arg_ind_in_flock;
-        std::tie(flock_ind, arg_ind_in_flock) = _mapping[argument];
-        std::vector<double> &flock = _distribution[flock_ind];
-        unsigned int num_of_args = _sizes[flock_ind];
-
-        for (unsigned int i = 0; i < flock.size(); i++) {
-            boost::dynamic_bitset<> binary(num_of_args, i);
-            if (binary.test(arg_ind_in_flock) == positive) {
-                unsigned int opposite = (unsigned int) binary.flip(arg_ind_in_flock).to_ulong();
-                double opposite_val = flock[opposite];
-                flock[opposite] *= (1 - coefficient);
-                flock[i] += coefficient * opposite_val;
+    std::unordered_map<unsigned int, double> Distribution::operator[](const std::vector<unsigned int> &arguments) const {
+        std::unordered_map<unsigned int, double> beliefs;
+        std::shared_ptr<Flock> flock = nullptr;
+        for(auto arg : arguments) {
+            if(_cache_is_valid.at(arg))
+                beliefs[arg] = _belief_cache.at(arg);
+            else {
+                if(flock == nullptr)
+                    flock = find_and_combine(arguments);
+                beliefs[arg] = flock->marginalize({arg})->distribution()[1];
             }
         }
+
+        return beliefs;
+    }
+
+    void Distribution::find_conditioning(unsigned int argument, std::set<unsigned int> &conditioning) const {
+        auto f = _flocks[_mapping.at(argument)];
+        for(auto cond : f->conditioning()) {
+            conditioning.insert(cond);
+            find_conditioning(cond, conditioning);
+        }
+    }
+
+    std::shared_ptr<Flock> Distribution::find_and_combine(const std::vector<unsigned int> &arguments) const {
+        std::set<unsigned int> conditioning_args;
+        std::set<std::shared_ptr<Flock>> conditioning_flocks;
+        for(auto arg : arguments) {
+            find_conditioning(arg, conditioning_args);
+            conditioning_flocks.insert(_flocks[_mapping.at(arg)]);
+        }
+        for(auto arg : conditioning_args)
+            conditioning_flocks.insert(_flocks[_mapping.at(arg)]);
+
+        std::deque<std::shared_ptr<Flock>> flock_deque(conditioning_flocks.cbegin(), conditioning_flocks.cend());
+        auto combined = flock_deque.front();
+        flock_deque.pop_front();
+        unsigned int limit = std::numeric_limits<unsigned int>::digits - 2;
+        while(!flock_deque.empty()) {
+            auto next = flock_deque.front();
+            if(combined->size() + next->size() > limit) {
+                auto all = std::vector<unsigned int>(arguments);
+                all.insert(all.cend(), conditioning_args.cbegin(), conditioning_args.cend());
+                combined = combined->marginalize(all);
+                next = next->marginalize(all);
+            }
+            combined = combined->combine(next);
+            flock_deque.pop_front();
+        }
+
+        return combined->marginalize(arguments);
     }
 
     std::string Distribution::to_str() const {
         std::stringstream ss;
-        for (auto distribution : _distribution)
-            for (double val : distribution) ss << val << " ";
-        return ss.str();
-    }
-
-    double Distribution::operator[](const unsigned int &argument) const {
-
-    }
-
-    std::ostream &operator<<(std::ostream &os, const Distribution &dist) {
-        os << dist.to_str() << std::endl;
-        return os;
+        for (auto flock : _flocks)
+            ss << flock->to_str() << " ";
+        std::string s = ss.str();
+        s.pop_back();
+        return s;
     }
 }
 
--- tests/distribution_test.cpp
+++ tests/distribution_test.cpp
@@ -1,40 +1,171 @@
+#include <memory>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "distribution.h"
+
+using ::testing::ElementsAreArray;
+using ::testing::DoubleEq;
+using ::testing::StrEq;
 
 class DistributionTest: public ::testing::Test {
 public:
     virtual void SetUp() {
-        dist = new splittercell::Distribution({splittercell::Flock({0,1}, {0})});
-        dist->set_flock_probabilities(0, {0.1, 0.2, 0.1, 0.6});
+        auto f1 = std::make_shared<splittercell::Flock>(std::vector<unsigned int>({0,1}));
+        dist = new splittercell::Distribution({f1});
+        dist->set_probabilities(0, {0.1, 0.2, 0.1, 0.6});
+
+        auto f2 = std::make_shared<splittercell::Flock>(std::vector<unsigned int>({0,1}), std::vector<unsigned int>({2}));
+        f2->set_probabilities({0.1, 0.0, 0.0, 0.2, 0.5, 0.0, 0.1, 0.1});
+        conditioned = new splittercell::Distribution({f2});
+
+        auto f3 = std::make_shared<splittercell::Flock>(std::vector<unsigned int>({2,3}), std::vector<unsigned int>({4}));
+        auto f4 = std::make_shared<splittercell::Flock>(std::vector<unsigned int>({0,1}), std::vector<unsigned int>({2}));
+        auto f5 = std::make_shared<splittercell::Flock>(std::vector<unsigned int>({4}));
+        threeflocks = new splittercell::Distribution({f4, f3, f5});
+        threeflocks->set_probabilities(0, {0.2, 0.0, 0.0, 0.8, 0.7, 0.0, 0.15, 0.15});
+        threeflocks->set_probabilities(1, {0.2, 0.0, 0.0, 0.8, 0.7, 0.0, 0.15, 0.15});
     }
 
     virtual void TearDown() {
         delete dist;
+        delete conditioned;
+        delete threeflocks;
     }
 
-    splittercell::Distribution *dist;
+    splittercell::Distribution *dist, *conditioned, *threeflocks;
 };
 
 TEST_F(DistributionTest, Refine1A) {
-    EXPECT_STREQ(dist->to_str().c_str(), "0.1 0.2 0.1 0.6 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
     dist->refine(0, true, 1.0);
-    EXPECT_STREQ(dist->to_str().c_str(), "0 0.3 0 0.7 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0 0.3 0 0.7"));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+    conditioned->refine(0, true, 1.0);
+    EXPECT_THAT(conditioned->to_str(), StrEq("0 0.1 0 0.2 0 0.5 0 0.2"));
 }
 
 TEST_F(DistributionTest, Refine1NotA) {
-    EXPECT_STREQ(dist->to_str().c_str(), "0.1 0.2 0.1 0.6 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
     dist->refine(0, false, 1.0);
-    EXPECT_STREQ(dist->to_str().c_str(), "0.3 0 0.7 0 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.3 0 0.7 0"));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+    conditioned->refine(0, false, 1.0);
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0.2 0 0.5 0 0.2 0"));
 }
 
 TEST_F(DistributionTest, Refine75A) {
-    EXPECT_STREQ(dist->to_str().c_str(), "0.1 0.2 0.1 0.6 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
     dist->refine(0, true, 0.75);
-    EXPECT_STREQ(dist->to_str().c_str(), "0.025 0.275 0.025 0.675 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.025 0.275 0.025 0.675"));
 }
 
 TEST_F(DistributionTest, Refine1B) {
-    EXPECT_STREQ(dist->to_str().c_str(), "0.1 0.2 0.1 0.6 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
     dist->refine(1, true, 1.0);
-    EXPECT_STREQ(dist->to_str().c_str(), "0 0 0.2 0.8 ");
+    EXPECT_THAT(dist->to_str(), StrEq("0 0 0.2 0.8"));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+    conditioned->refine(1, true, 1.0);
+    EXPECT_THAT(conditioned->to_str(), StrEq("0 0 0.1 0.2 0 0 0.6 0.1"));
+}
+
+TEST_F(DistributionTest, MarginalizeA) {
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
+    auto m = dist->marginalize(0, {0, 4})->distribution(); //To test that non included arguments are ignored
+    EXPECT_THAT(m, ElementsAreArray({DoubleEq(0.2), DoubleEq(0.8)}));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+    m = conditioned->marginalize(0, {0})->distribution();
+    EXPECT_THAT(m, ElementsAreArray({DoubleEq(0.1), DoubleEq(0.2), DoubleEq(0.6), DoubleEq(0.1)}));
+}
+
+TEST_F(DistributionTest, MarginalizeB) {
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
+    auto m = dist->marginalize(0, {1})->distribution();
+    EXPECT_THAT(m, ElementsAreArray({DoubleEq(0.3), DoubleEq(0.7)}));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+    m = conditioned->marginalize(0, {1})->distribution();
+    EXPECT_THAT(m, ElementsAreArray({DoubleEq(0.1), DoubleEq(0.2), DoubleEq(0.5), DoubleEq(0.2)}));
+}
+
+TEST_F(DistributionTest, MarginalizeAB) {
+    EXPECT_THAT(dist->to_str(), StrEq("0.1 0.2 0.1 0.6"));
+    auto m = dist->marginalize(0, {0,1})->distribution();
+    EXPECT_THAT(m, ElementsAreArray({DoubleEq(0.1), DoubleEq(0.2), DoubleEq(0.1), DoubleEq(0.6)}));
+
+    EXPECT_THAT(conditioned->to_str(), StrEq("0.1 0 0 0.2 0.5 0 0.1 0.1"));
+}
+
+TEST_F(DistributionTest, Combine) {
+    auto m = threeflocks->flock(0)->combine(threeflocks->flock(1));
+    EXPECT_THAT(m->distribution(), ElementsAreArray({DoubleEq(0.04), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.16),
+                                     DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0),
+                                     DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.0),
+                                     DoubleEq(0.56), DoubleEq(0.0), DoubleEq(0.12), DoubleEq(0.12),
+                                     DoubleEq(0.14), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.56),
+                                     DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.0),
+                                     DoubleEq(0.03), DoubleEq(0.0), DoubleEq(0.0), DoubleEq(0.12),
+                                     DoubleEq(0.105), DoubleEq(0.0), DoubleEq(0.0225), DoubleEq(0.0225)}));
+}
+
+TEST_F(DistributionTest, InitialBeliefA) {
+    auto beliefs = (*dist)[{0}];
+    EXPECT_THAT(beliefs[0], DoubleEq(0.8));
+}
+
+TEST_F(DistributionTest, InitialBeliefB) {
+    auto beliefs = (*dist)[{1}];
+    EXPECT_THAT(beliefs[1], DoubleEq(0.7));
+}
+
+TEST_F(DistributionTest, InitialBeliefAB) {
+    auto beliefs = (*dist)[{0,1}];
+    EXPECT_THAT(beliefs[0], DoubleEq(0.8));
+    EXPECT_THAT(beliefs[1], DoubleEq(0.7));
+}
+
+TEST_F(DistributionTest, ThreeSplitA) {
+    EXPECT_THAT(threeflocks->operator[]({0})[0], DoubleEq(0.49125));
+}
+
+TEST_F(DistributionTest, ThreeSplitD) {
+    EXPECT_THAT(threeflocks->operator[]({3})[3], DoubleEq(0.55));
+}
+
+TEST_F(DistributionTest, ThreeSplitC) {
+    EXPECT_THAT(threeflocks->operator[]({2})[2], DoubleEq(0.475));
+}
+
+TEST_F(DistributionTest, ThreeSplitE) {
+    EXPECT_THAT(threeflocks->operator[]({4})[4], DoubleEq(0.5));
+}
+
+TEST(StressTest, HugeRefinement) {
+    std::vector<unsigned int> args;
+    for(unsigned int i = 0; i < 25; i++)
+        args.push_back(i);
+    splittercell::Flock f(args);
+    f.refine(0, true, 1);
+}
+
+TEST(StressTest, HugeMarginalization) {
+    std::vector<unsigned int> args;
+    for(unsigned int i = 0; i < 25; i++)
+    args.push_back(i);
+    splittercell::Flock f(args);
+    f.marginalize({0});
+}
+
+TEST(StressTest, HugeCombination) {
+    std::vector<unsigned int> args1, args2;
+    for(unsigned int i = 0; i < 12; i++)
+        args1.push_back(i);
+    for(unsigned int i = 12; i < 25; i++)
+        args2.push_back(i);
+    splittercell::Flock f1(args1, std::vector<unsigned int>({12}));
+    auto f2 = std::make_shared<splittercell::Flock>(args2);
+    f1.combine(f2);
 }
